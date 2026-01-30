e489dd3aab319be42421bdb972aaa8310aae9e55
Padmalochan <padmalochansutar98@gmail.com>
2026-01-30 15:51:53 +0530

    code changes

--- src/main/java/com/example/demo/KafkaDeliveryBoyApplication.java
+++ src/main/java/com/example/demo/KafkaDeliveryBoyApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaDeliveryBoyApplication {
-
+    Map<string,Object>  map=new HashMap<>();
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaDeliveryBoyApplication.class, args);
 	}
-
+ public void loadDataToCach(){
+		System.out.println("Data store in cache");
+     }
 }
